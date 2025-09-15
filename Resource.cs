af607bdd58c268293887e9f4527468e633da6854
Alex Justi <alex@justiemergencytech.com>
2025-09-15 01:12:36 -0500

    Add Resource class

---
+++ Resource.cs
@@ -0,0 +1,16 @@
+﻿namespace NCAP;
+
+public class Resource
+{
+    public string Description { get; set; }
+
+    public string MimeType { get; set; }
+
+    public int? Size { get; set; }
+
+    public string? Uri { get; set; }
+
+    public string? DerefUri { get; set; }
+
+    public string? Digest { get; set; }
+}
