9bf6f5dc3494d5ceb842a1797daa9e83828841e5
Alex Justi <alex@justiemergencytech.com>
2025-09-25 23:22:24 -0500

    Add NWS Alerting definitions

---
+++ Specialty/Nws/BlockChannel.cs
@@ -0,0 +1,9 @@
+﻿namespace NCAP.Specialty.Nws;
+
+public enum BlockChannel
+{
+    CMAS,   //Wireless Emergency Alerts
+    EAS,    //Emergency Alert System
+    NWEM,   //Non-Weather Emergency Message
+    PUBLIC  //IPAWS Public Alert Dissemination Channel
+}
