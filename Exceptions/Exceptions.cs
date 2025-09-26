3065607fcf4aa6eea128de3449c1bf4d86620997
Alex Justi <alex@justiemergencytech.com>
2025-09-25 23:39:42 -0500

    Add deserialization functions (initial)

--- Exceptions/Exceptions.cs
+++ Exceptions/Exceptions.cs
@@ -5,3 +5,13 @@ public class InvalidCircleException : Exception;
 public class InvalidPolygonException : Exception;
 
 public class RestrictedCharacterException : Exception;
+
+public class AlertSerializationException : Exception
+{
+    public override string Message => "An error occurred while attempting to serialize an alert.";
+}
+
+public class AlertDeserializationException : Exception
+{
+    public override string Message => "An error occurred while attempting to deserialize an alert.";
+}
--- Xml/AlertDeserializer.cs
+++ Xml/AlertDeserializer.cs
@@ -1,6 +1,38 @@
-﻿namespace NCAP.Xml;
+﻿using System.Text;
+using System.Xml.Serialization;
+using NCAP.Exceptions;
+
+namespace NCAP.Xml;
 
 public class AlertDeserializer
 {
-    
+    public static Alert DeserializeAlert(Stream stream)
+    {
+        //An empty stream cannot be deserialized
+        if (stream.Length == 0)
+            throw new AlertDeserializationException();
+
+        try
+        {
+            var serializer = new XmlSerializer(typeof(Alert));
+
+            return (Alert)serializer.Deserialize(stream)!;
+        }
+        catch (Exception ex)
+        {
+            throw new AlertDeserializationException();
+        }
+    }
+
+    public static Alert DeserializeAlert(string xmlAlert)
+    {
+        var stream = new MemoryStream();
+        var writer = new StreamWriter(stream, Encoding.UTF8);
+
+        writer.Write(xmlAlert);
+        writer.Flush();
+        stream.Position = 0;
+
+        return DeserializeAlert(stream);
+    }
 }
