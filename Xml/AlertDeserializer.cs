d4b9c575dab38d53c0e4cecdfdb252f248f23b9d
Alex Justi <alex@justiemergencytech.com>
2025-10-04 23:02:21 -0500

    Add methods to deserialize multiple alerts

--- Xml/AlertDeserializer.cs
+++ Xml/AlertDeserializer.cs
@@ -7,7 +7,7 @@ namespace NCAP.Xml;
 
 public class AlertDeserializer
 {
-    public static Alert DeserializeAlert(Stream stream)
+    public static Alert? DeserializeAlert(Stream stream)
     {
         //An empty stream cannot be deserialized
         if (stream.Length == 0)
@@ -25,15 +25,46 @@ public class AlertDeserializer
         }
     }
 
-    public static Alert DeserializeAlert(string xmlAlert)
+    public static IEnumerable<Alert>? DeserializeAlerts(Stream stream)
+    {
+        if (stream.Length == 0)
+            throw new AlertDeserializationException();
+
+        try
+        {
+            var serializer = new XmlSerializer(
+                typeof(List<Alert>),
+                new XmlRootAttribute("alerts"));
+
+            return serializer.Deserialize(stream) as List<Alert>;
+        }
+        catch (Exception ex)
+        {
+            throw new AlertDeserializationException();
+        }
+    }
+
+    public static Alert? DeserializeAlert(string xml)
     {
         var stream = new MemoryStream();
         var writer = new StreamWriter(stream, Encoding.UTF8);
 
-        writer.Write(xmlAlert);
+        writer.Write(xml);
         writer.Flush();
         stream.Position = 0;
 
         return DeserializeAlert(stream);
+    }
+
+    public static IEnumerable<Alert>? DeserializeAlerts(string xml)
+    {
+        var stream = new MemoryStream();
+        var writer = new StreamWriter(stream, Encoding.UTF8);
+
+        writer.Write(xml);
+        writer.Flush();
+        stream.Position = 0;
+
+        return DeserializeAlerts(stream);
     }
 }
